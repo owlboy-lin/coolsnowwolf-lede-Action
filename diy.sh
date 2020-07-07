d39e7a306f4cd96f4c7046775a1ff6793b4456a2
owlboy-lin <60811203+owlboy-lin@users.noreply.github.com>
2020-07-07 09:52:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,7 +12,7 @@
 # git clone https://github.com/Lienol/openwrt-package.git package/lienol
 
 # Add a feed source
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
 
